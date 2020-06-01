8e70dafe2c472f940db8fb342bdcd0142cd2b103
Ryan Hileman <lunixbochs@gmail.com>
2020-05-31 21:49:41 -0700

    add indices

--- server/app/app.py
+++ server/app/app.py
@@ -12,3 +12,17 @@ app.config.update({
 if 'FLASK_SETTINGS' in os.environ:
     app.config.from_envvar('FLASK_SETTINGS')
 mongo = PyMongo(app)
+
+@app.before_first_request
+def setup_indexes():
+    indexes = {
+        'feeds': [('numeric_id', 1)],
+        'calls': [[('ts', -1), ('feed_id', 1)]],
+    }
+    for name, items in indexes.items():
+        col = mongo.db[name]
+        for index in items:
+            kwargs = {}
+            if not isinstance(index, list):
+                index = [index]
+            col.ensure_index(index, background=True, **kwargs)
